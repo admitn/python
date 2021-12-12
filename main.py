1b405173000881170781f504e9a0ba0bc788f8bb
admitn <admitn91@gmail.com>
2021-12-12 22:45:48 +0300

    python
    python

---
+++ main.py
@@ -0,0 +1,20 @@
+import csv
+import json
+
+path = 'test.json'
+path2 = 'test2.json'
+
+arr = []
+with open(path, 'r') as f:
+    data = json.load(f)
+arr.append(data.values())
+
+with open(path2, 'r') as f:
+    data2 = json.load(f)
+arr.append(data2.values())
+
+with open('test.csv', 'w') as f:
+    csv_wr = csv.writer(f)
+    csv_wr.writerow(data)
+    for w in arr:
+        csv_wr.writerow(w)
